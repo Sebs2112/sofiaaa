005cd4bc365800516576fd263edf1f8fc8511f45
Sebs2112 <sebs2112@gmail.com>
2019-11-06 09:59:41 +0000

    Update the env variables

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
+console.log("Hi")
+console.log(process.env.MONGODB)
 
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
