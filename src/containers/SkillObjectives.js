8b9b8639bc277a695eb0e0ff2bb7d73b3458ccbe
Sebs2112 <sebs2112@gmail.com>
2019-11-19 09:11:05 +0000

    Refactored SkillObjectives

--- src/containers/SkillObjectives.js
+++ src/containers/SkillObjectives.js
@@ -2,82 +2,77 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 function SkillObjectives({ skillsChosen, skills, ...props }) {
+  const theSkills = skillsChosen
+    ? skillsChosen.map(skill => {
+        return skills.filter(s => s.skillName === skill);
+      })
+    : [];
 
-const theSkills = skillsChosen ? skillsChosen.map(skill => { 
-    return skills.filter(s => s.skillName === skill)
-}) : []
+function tableColumns(skill,levelNo){
+    return(
+        <td>
+            {skill[0].skillLevels.filter(lev => lev.level === levelNo)[0] ? (
+              <ul>
+                {skill[0].skillLevels
+                  .filter(lev => lev.level === levelNo)[0]
+                  .objectives.map(obj => {
+                    return <li>{obj.objective}</li>;
+                  })}
+              </ul>
+            ) : (
+              "No content"
+            )}
+          </td>
+    )
+}
 
-const skillsToTable = theSkills ? theSkills.map(skill => { 
+  const skillsToTable = theSkills ? (
+    theSkills.map(skill => {
+      return (
+        <tr>
+          <th>{skill[0].skillName}</th>
+          {tableColumns(skill,1)}
+          {tableColumns(skill,2)}
+          {tableColumns(skill,3)}
+          {tableColumns(skill,4)}
+          {tableColumns(skill,5)}
+          {tableColumns(skill,6)}
+          {tableColumns(skill,7)}
+        </tr>
+      );
+    })
+  ) : (
+    <tr></tr>
+  );
 
-    console.log(skill[0].skillLevels.filter(lev=> lev.level === 4)[0].objectives.map(obj => {return obj.objective}))
+  // console.log(theSkills[0][0])
 
-    return ( <tr>   
-                <th>{skill[0].skillName}</th>
-                <td>
-                    {skill[0].skillLevels.filter(lev=> lev.level === 1)[0] ? 
-                    skill[0].skillLevels.filter(lev=> lev.level === 1)[0].objectives.map(obj => {return obj.objective})
-                    : "No content"}
-                </td>
-                <td>
-                    {skill[0].skillLevels.filter(lev=> lev.level === 2)[0] ? 
-                    skill[0].skillLevels.filter(lev=> lev.level === 2)[0].objectives.map(obj => {return obj.objective})
-                    : "No content"}
-                </td>
-                <td>
-                    {skill[0].skillLevels.filter(lev=> lev.level === 3)[0] ? 
-                    skill[0].skillLevels.filter(lev=> lev.level === 3)[0].objectives.map(obj => {return obj.objective})
-                    : "No content"}
-                </td>
-                <td>
-                    {skill[0].skillLevels.filter(lev=> lev.level === 4)[0] ? 
-                    skill[0].skillLevels.filter(lev=> lev.level === 4)[0].objectives.map(obj => {return obj.objective})
-                    : "No content"}
-                </td>
-                <td>
-                    {skill[0].skillLevels.filter(lev=> lev.level === 5)[0] ? 
-                    skill[0].skillLevels.filter(lev=> lev.level === 5)[0].objectives.map(obj => {return obj.objective})
-                    : "No content"}
-                </td>
-                <td>
-                    {skill[0].skillLevels.filter(lev=> lev.level === 6)[0] ? 
-                    skill[0].skillLevels.filter(lev=> lev.level === 6)[0].objectives.map(obj => {return obj.objective})
-                    : "No content"}
-                </td>
-                <td>
-                    {skill[0].skillLevels.filter(lev=> lev.level === 7)[0] ? 
-                    skill[0].skillLevels.filter(lev=> lev.level === 7)[0].objectives.map(obj => {return obj.objective})
-                    : "No content"}
-                </td>
-                </tr>
-                 )
-}) : <tr></tr>
-
-// console.log(theSkills[0][0])
-
-
-
-  return <div className="container" style =  {{
-    backgroundColor: "#41669d",
-    overflowX: "scroll",
-    minHeight: "70vh"
-  }}> 
-  <thead>
-    <tr>
-      <th scope="col">Skill</th>
-      <th scope="col">Level 1</th>
-      <th scope="col">Level 2</th>
-      <th scope="col">Level 3</th>
-      <th scope="col">Level 4</th>
-      <th scope="col">Level 5</th>
-      <th scope="col">Level 6</th>
-      <th scope="col">Level 7</th>
-
-    </tr>
-  </thead>
-  <tbody>
-      {skillsToTable}
-  </tbody>
-  </div>;
+  return (
+    <div
+      className="container"
+      style={{
+        backgroundColor: "#41669d",
+        overflowX: "scroll",
+        minHeight: "70vh"
+      }}
+    >
+      <table class="table"    style= {{tableLayout:"fixed", width:"2000px"}}>
+        <thead>
+          <tr>
+            <th scope="col"  >Skill</th>
+            <th scope="col" >Level 1</th>
+            <th scope="col" >Level 2</th>
+            <th scope="col" >Level 3</th>
+            <th scope="col">Level 4</th>
+            <th scope="col" >Level 5</th>
+            <th scope="col" >Level 6</th>
+            <th scope="col" >Level 7</th>
+          </tr>
+        </thead>
+        <tbody>{skillsToTable}</tbody>
+      </table>
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
